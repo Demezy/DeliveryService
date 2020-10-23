6498463e5cf3c9cf8e53018a414e47c4556d6714
Demezy <arbuzik.arbuz0v555@gmail.com>
2020-10-24 01:24:18 +0200

    Integrate postgres
    Integrate postgres, made stub class for site, preparation for login system

--- DeliveryService/main/models.py
+++ DeliveryService/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import random
+from hashlib import sha256
 
-# Create your models here.
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+
+def key_gen():
+    password = ''
+
+    for i in range(10):
+        password += random.choice(chars)
+
+    return password
+
+
+class User(models.Model):
+    id = models.AutoField('id', primary_key=True, auto_created=True)
+    username = models.CharField('username', max_length=100, unique=True)
+    email = models.CharField('email', max_length=100, blank=True)
+    password = models.TextField('password', blank=True)
+    invitation_code = models.TextField('invitation_code', default=key_gen)
+
+    def __str__(self):
+        return f'Name: {self.username}{" " * 4}Email:{self.email}{" " * 4}UserType:{self.invitation_code}'
--- DeliveryService/main/views.py
+++ DeliveryService/main/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+from .models import User
+
+# constants
+COMPANY = "Delivery Service"
+
+
 # from django.http import HttpResponse
+# todo replace TEMP to DB
+class TEMP:
+    """temporary class for test"""
+
+    def __init__(self, num: str) -> None:
+        self.title = "TestTitle" + num
+        self.text = "Text" + num
+        self.count = num
+        self.img = "super.png"
+
+    def __str__(self):
+        return "STRING"
+
+
+products = [TEMP(str(i)) for i in range(10)]
 
 
 def index(request):
     # return HttpResponse("<h1> Index page </h1>")
+    # data = User.objects.get(id=1) # example
     return render(request, "main/index.html", {"title": "Home"})
 
 
 def test_page(request):
-    return render(request, "main/test.html")
+    return render(request, "main/test.html", {"products": products, "COMPANY": COMPANY})
