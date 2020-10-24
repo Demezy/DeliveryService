5d284e43a03df125c89f1f248de3fe7c888b1bf3
Demezy <arbuzik.arbuz0v555@gmail.com>
2020-10-24 11:53:24 +0200

    Create DB with products
    create and integrate product database, erase TEMP, minor cosmetic changes

--- DeliveryService/main/models.py
+++ DeliveryService/main/models.py
@@ -2,16 +2,12 @@ from django.db import models
 import random
 from hashlib import sha256
 
-chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+# these symbols are used to generate passwords
+CHARS = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
-
-def key_gen():
-    password = ''
-
-    for i in range(10):
-        password += random.choice(chars)
-
-    return password
+# TODO: rename keygen to pass_gen
+def key_gen() -> str:
+    return "".join([random.choice(CHARS) for i in range(10)])
 
 
 class User(models.Model):
@@ -23,3 +19,16 @@ class User(models.Model):
 
     def __str__(self):
         return f'Name: {self.username}{" " * 4}Email:{self.email}{" " * 4}UserType:{self.invitation_code}'
+
+
+class Product(models.Model):
+    id = models.AutoField('id', primary_key=True, auto_created=True)
+    title = models.CharField("title", max_length=100, blank=True, unique=True)
+    text = models.TextField("text", max_length=300, blank=True)
+    count = models.IntegerField(null=False, default=0)
+    img = models.CharField("img", max_length=100, blank=True)
+
+    # TODO: validation for empty img
+
+    def __str__(self):
+        return f"{self.title}\t -- {self.count}"
--- DeliveryService/main/views.py
+++ DeliveryService/main/views.py
@@ -1,33 +1,15 @@
 from django.shortcuts import render
-from .models import User
+from .models import User, Product
 
 # constants
 COMPANY = "Delivery Service"
 
 
-# from django.http import HttpResponse
-# todo replace TEMP to DB
-class TEMP:
-    """temporary class for test"""
-
-    def __init__(self, num: str) -> None:
-        self.title = "TestTitle" + num
-        self.text = "Text" + num
-        self.count = num
-        self.img = "super.png"
-
-    def __str__(self):
-        return "STRING"
-
-
-products = [TEMP(str(i)) for i in range(10)]
-
-
 def index(request):
     # return HttpResponse("<h1> Index page </h1>")
     # data = User.objects.get(id=1) # example
-    return render(request, "main/index.html", {"title": "Home"})
+    return render(request, "main/index.html", {"products": Product.objects.all(), "COMPANY": COMPANY})
 
 
 def test_page(request):
-    return render(request, "main/test.html", {"products": products, "COMPANY": COMPANY})
+    return render(request, "main/test.html", {"products": Product.objects.all(), "COMPANY": COMPANY})
