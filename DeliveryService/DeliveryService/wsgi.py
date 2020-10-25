138327b71a9a0342c3ead1bb1dabf229b236c8a3
Demezy <arbuzik.arbuz0v555@gmail.com>
2020-10-25 18:50:23 +0200

    minor
    add subs for future functions, prepare manage_page, added favicon to base, text wrap in Shop.html. Restrict time per SQL request (wsgi.py)

--- DeliveryService/DeliveryService/wsgi.py
+++ DeliveryService/DeliveryService/wsgi.py
@@ -6,11 +6,26 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
+# standard
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeliveryService.settings')
+# additional
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeliveryService.settings')
 application = get_wsgi_application()
+
+
+# time restriction for SQL request
+@receiver(connection_created)
+def setup_postgres(connection, **kwargs):
+    if connection.vendor != 'postgresql':
+        return
+
+    # time out 30 seconds.
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SET statement_timeout TO 30000;
+        """)
