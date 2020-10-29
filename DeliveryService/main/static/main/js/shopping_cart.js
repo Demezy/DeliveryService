cfcada9011f8508100387561897946ce9c78c37f
Demezy <arbuzik.arbuz0v555@gmail.com>
2020-10-29 02:14:18 +0200

    rework of shop
    js module isn't work yet,time is limited, so there is a new method

--- DeliveryService/main/static/main/js/shopping_cart.js
+++ DeliveryService/main/static/main/js/shopping_cart.js
@@ -1,30 +1,77 @@
 console.log("Shopping cart is ready");
+
+let productList = {};
+let basket = document.querySelector("span.basket");
 let carts = document.querySelectorAll(".add-cart");
 let delCarts = document.querySelectorAll(".remove-cart");
 
+// let btns = document.querySelectorAll(".btn-group")
+// console.log(btns[1].children)
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener("click", () => {
-        cartNumbers();
+        cartNumbers(carts[i]);
     });
     delCarts[i].addEventListener("click", () => {
-        removeCart();
+        removeCart(carts[i]);
     });
+    // console.log(carts[i])
+    productList[carts[i].getAttribute("value")] = {
+        "price": carts[i].parentElement.children[0].value,
+        "inCart": 0
+    };
+    // console.log(carts[i].getAttribute("value"));
+    // console.log(carts[i].parentElement.children[0])
 }
 
-let basket = document.querySelector("span.basket")
+// localStorage[product.value] = parseInt(localCart[product.value]) + 1
 
-function removeCart() {
-    localStorage.setItem("cartNumbers", 0);
+function removeCart(product) {
+    localStorage.removeItem(product.value);
     basket.textContent = 0;
 }
 
-function cartNumbers() {
-    productNumbers = parseInt(localStorage.getItem("cartNumbers"));
+function cartNumbers(product) {
+    console.log(product.value);
+    let productNumbers = parseInt(localStorage.getItem("countOfProducts"));
     if (productNumbers) {
-        localStorage.setItem("cartNumbers", productNumbers + 1);
-        basket.textContent = productNumbers + 1;
+        localStorage.setItem("countOfProducts", productNumbers + 1);
+
     } else {
-        localStorage.setItem("cartNumbers", 1);
-        basket.textContent = 1;
+        localStorage.setItem("countOfProducts", 1);
     }
+    basket.textContent = productNumbers + 1;
+    setItem(product);
 }
+
+function setItem(product) {
+    console.log(product.value);
+    let localCart = JSON.parse(localStorage.getItem("localCart"));
+    if (localCart == null) {
+        localCart = {product.value
+    :
+        {
+            "count"
+        :
+            product.inCart, "price"
+        :
+            product.price
+        }
+    }
+    } else {
+        if (localCart[value].inCart != null) {
+            localCart = {
+                ...localCart,
+                [value]: product
+            }
+        }
+        localCart[value].inCart += 1
+    }
+
+// parseInt(localCart[product.value]) + 1
+    console.log(localCart)
+    localStorage.setItem("localCart", JSON.stringify(localCart))
+}
+
+// console.log("shopping_cart.js");
+// console.log(productList);
+// productList["test2"] = 21321
